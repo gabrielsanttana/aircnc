8799fc7893b00d162e72442ebdd33d375b387ff2
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-26 18:53:57 -0300

    Update database connection string

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-lfmbb.mongodb.net/aircnc`, {
+mongoose.connect(`mongodb+srv://...`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
